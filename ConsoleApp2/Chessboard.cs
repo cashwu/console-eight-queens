2c6f3db9c9a60a29a4f70b86e6450f449bc2c819
嬋娟 葉 <s30122@hotmail.com>
2020-12-22 23:30:37 +0800

    Add project files.

---
+++ ConsoleApp2/Chessboard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Chessboard
+    {
+       
+        public Chessboard()
+        {
+            CanPutPositions = new List<KeyValuePair<int, int>>();
+            for (int j = 1; j <= 8; j++)
+            {
+                for (int i = 1; i <=8; i++)
+                {
+                    CanPutPositions.Add(new KeyValuePair<int, int>(i,j));
+                }
+            }
+            QueenPositions = new List<KeyValuePair<int, int>>();
+        }
+        public Chessboard(Chessboard board,KeyValuePair<int,int> position)
+        {
+            CanPutPositions = board.CanPutPositions.Except(new[] { position }).ToList();
+            QueenPositions= new List<KeyValuePair<int, int>>(board.QueenPositions.Concat(new [] { position }));
+            for (int y = 1; y <=8 ; y++)
+            {
+                for (int x = 1; x <= 8; x++)
+                {
+                    if (x == position.Key || //橫線 
+                        y == position.Value || //直線
+                        x+y == position.Key + position.Value ||//左下到右上 
+                        x-y == position.Key - position.Value //左上到右下
+                        )                   
+                    {
+                        if (CanPutPositions.Where(can => can.Key == x && can.Value == y).Any())
+                        {
+                            CanPutPositions.Remove(new KeyValuePair<int, int>(x,y));
+                        }
+                    }
+                   
+                }
+            }
+
+        }
+        public List<KeyValuePair<int, int>> CanPutPositions { get; set; }
+        public List<KeyValuePair<int, int>> QueenPositions { get; set; }
+        public bool QueenCountEqualEight => QueenPositions.Count == 8;
+        public bool NoSolution => CanPutPositions.Count < 8 - QueenPositions.Count;
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+
+            var board = new Chessboard();
+            Explore(board);
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+        static int resIdx = 1;
+        private static void Explore(Chessboard board)
+        {
+            var firstQueen = board.QueenPositions.Any()? board.QueenPositions.Last() : new KeyValuePair<int, int>(0,0);
+            foreach (var position in board.CanPutPositions.Where(x=>x.Key*10+x.Value>firstQueen.Key*10+firstQueen.Value))
+            {
+                var newBoard = new Chessboard(board, position);
+                
+                if (newBoard.QueenCountEqualEight)
+                {
+                    Console.WriteLine($"success：{resIdx++}");
+                    
+                    for (int y = 1; y <= 8; y++)
+                    {
+                        var sb = new StringBuilder();
+                        for (int x = 1; x <= 8; x++)
+                        {
+                            if (newBoard.QueenPositions.Any(a => a.Key == x && a.Value == y))
+                            {
+                                sb.Append("Q");
+                            }
+                            else
+                            {
+                                sb.Append(".");
+                            }
+                            
+                        }
+                        Console.WriteLine(sb.ToString());
+                    }
+                    Console.WriteLine("================");
+                
+                }
+                else if (!newBoard.NoSolution)
+                {
+                    Explore(newBoard);
+                }
+            }
+        }
+    }
+}
